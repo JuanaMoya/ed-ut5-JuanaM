820de019173fa14f92e5db2d4853da4aaeffd689
Juana Moya <j.moya@edu.gva.es>
2023-11-26 12:41:41 +0100

    Comentario sobre el main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!"
-        );
+//commentary MAin
+        System.out.println("Hello world!");
     }
 }
